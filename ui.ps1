83c5902daf8379f9454de513b948ceb4fc445829
Lucas Dantas <56886569+lucasldantas@users.noreply.github.com>
2025-08-25 16:51:31 -0300

    Update ui.ps1

--- ui.ps1
+++ ui.ps1
@@ -173,7 +173,7 @@ $BtnDelay1.Add_Click({
   try {
     $runAt = (Get-Date).AddHours(1)
     New-RePromptTask -when $runAt
-    [System.Windows.MessageBox]::Show("Agendado para $($runAt:dd/MM/yyyy HH:mm). A janela será reaberta nessa hora para confirmar a execução.", "Agendado", 'OK', 'Information') | Out-Null
+    [System.Windows.MessageBox]::Show("Agendado para $($runAt.ToString('dd/MM/yyyy HH:mm')). A janela será reaberta nessa hora para confirmar a execução.", "Agendado", 'OK', 'Information') | Out-Null
   } catch {
     [System.Windows.MessageBox]::Show("Falha ao agendar: `n$($_.Exception.Message)", "Erro", 'OK', 'Error') | Out-Null
   }
@@ -185,7 +185,7 @@ $BtnDelay2.Add_Click({
   try {
     $runAt = (Get-Date).AddHours(2)
     New-RePromptTask -when $runAt
-    [System.Windows.MessageBox]::Show("Agendado para $($runAt:dd/MM/yyyy HH:mm). A janela será reaberta nessa hora para confirmar a execução.", "Agendado", 'OK', 'Information') | Out-Null
+    [System.Windows.MessageBox]::Show("Agendado para $($runAt.ToString('dd/MM/yyyy HH:mm')). A janela será reaberta nessa hora para confirmar a execução.", "Agendado", 'OK', 'Information') | Out-Null
   } catch {
     [System.Windows.MessageBox]::Show("Falha ao agendar: `n$($_.Exception.Message)", "Erro", 'OK', 'Error') | Out-Null
   }
